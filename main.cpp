97cc1549941b5831ddb353acaa47d404cf1e411a
juan pablo sanchez gaitan <50275221+lordcidiak@users.noreply.github.com>
2020-10-25 16:55:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -121,8 +121,8 @@ void llenarListaCartas(sLista<sCarta *> *baraja, int i)
   else
   {
     int f = i / 20;
-    int v = (i % 10);
-    int y = (i % 9);
+    int v = (i % 9);
+    int y = (i % 10);
     sCarta *carta = new sCarta;
     carta->figura = new char[15];
     switch (f)
@@ -146,7 +146,7 @@ void llenarListaCartas(sLista<sCarta *> *baraja, int i)
     {
       carta->valor = y;
     }else{
-      carta->valor = v;
+      carta->valor = v+1;
     }
     
 
