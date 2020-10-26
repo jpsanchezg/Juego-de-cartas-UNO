d897857921c5c9d5ac12fb804ceea9743744d88e
juan pablo sanchez gaitan <50275221+lordcidiak@users.noreply.github.com>
2020-10-26 16:38:01 -0500

    arreglos en el main
    cambios en el main de cuestion de estetica, con introduccion a la libreria lista.h con lista.cpp que contienen todo lo relacionado con las listas

---
+++ lista.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include "lista.h"
+
+using namespace std;
+
+template <class T>
+sLista<T> *crearLista()
+{
+  sLista<T> *lista = new sLista<T>;
+  lista->cab = NULL;
+  lista->tam = 0;
+  return lista;
+}
+
+template <class T>
+bool listaVacia(sLista<T> *lista)
+{
+  if (lista->tam == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+template <class T>
+sNodo<T> *crearNodo(T dato)
+{
+  sNodo<T> *nodo = new sNodo<T>;
+  nodo->dato = dato;
+  nodo->sig = NULL;
+  return nodo;
+}
+
+template <class T>
+void insertarNodo(sLista<T> *lista, T valor)
+{
+  sNodo<T> *nodo = crearNodo(valor);
+  if (listaVacia(lista) == true)
+  {
+    lista->cab = nodo;
+    lista->cola = nodo;
+  }
+  else
+  {
+    lista->cola->sig = nodo;
+    lista->cola = nodo;
+  }
+  lista->tam++;
+}
+
+template <class T>
+void imprimirListaCartas(sLista<T> *lista)
+{
+  sNodo<T> *aux = lista->cab;
+  while (aux != NULL)
+  {
+    cout << aux->dato->valor << " " << aux->dato->figura << endl;
+    aux = aux->sig;
+  }
+}
+
+template <class I>
+void imprimirListaJugadores(sLista<I> *lista)
+{
+  sNodo<I> *aux = lista->cab;
+  while (aux != NULL)
+  {
+    cout << aux->dato->nombre << " " << aux->dato->apellido << endl;
+    aux = aux->sig;
+  }
+}
+
+template <typename X>
+void insertarNodoInicio(sLista<X> *lista, X persona)
+{
+  sNodo<X> *nodo = crearNodo<X>(persona);
+  if (listaVacia<X>(lista) == true)
+  {
+    lista->cab = nodo;
+  }
+  else
+  {
+    nodo->sig = lista->cab;
+    lista->cab = nodo;
+  }
+  lista->tam++;
+}
+
